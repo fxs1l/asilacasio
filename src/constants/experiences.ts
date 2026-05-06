654fc8c0d84153783ee6116ecf4ddc6a3d4e80ec
Asil Andrei Acasio <fx.silndrei@gmail.com>
2026-05-06 10:40:16 +0800

    feat: add new experiences

--- src/constants/experiences.ts
+++ src/constants/experiences.ts
@@ -20,6 +20,34 @@ export const MY_EXPERIENCES: Experience[] = [
     isFeatured: true,
   },
   {
+    title: "Data Scientist — Machine Learning Operations",
+    description:
+      "Executed the development and modernization of end-to-end Machine Learning pipelines within the ModelOps framework. I focused on building scalable infrastructure for model monitoring and continuous integration, ensuring the reliability of production-grade financial models for the Bank.",
+    type: ExperienceType.WORK,
+    company: {
+      name: "Bank of the Philippine Islands",
+      url: "https://www.bpi.com.ph/",
+    },
+    mode: WorkMode.ONSITE,
+    startDate: new Date("2026-05-13"),
+    endDate: new Date(),
+    isFeatured: false,
+  },
+  {
+    title: "Officership Training Program  ",
+    description:
+      "Completing a comprehensive training program focused on leadership development, organizational management, and effective communication. This experience equipped me with essential skills to lead and manage teams successfully.",
+    type: ExperienceType.WORK,
+    company: {
+      name: "Bank of the Philippine Islands",
+      url: "https://www.bpi.com.ph/",
+    },
+    mode: WorkMode.ONSITE,
+    startDate: new Date("2026-02-16"),
+    endDate: new Date(),
+    isFeatured: true,
+  },
+  {
     title: "Web and Mobile App Developer Intern",
     description:
       "Contributed to two projects involving frontend development using NextJs, backend API development with NestJS, and cloud configuration on Google Cloud Platform. This experience enhanced my technical skills and understanding of the web development lifecycle.",
